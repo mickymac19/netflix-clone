7577a8dbcf6fd09e7508c18d85fd1cee979105ea
Michelle Macato <michellefmacato@gmail.com>
2020-09-07 17:04:48 +0900

    added styling to banner description and buttons

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import requests from './request';
+import './Banner.css';
 
 function Banner() {
     const [movie, setMovie] = useState([]); 
@@ -19,6 +20,10 @@ function Banner() {
     }, []); 
 console.log(movie); 
 
+function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." :str;
+}
+
     return (
         <header className="banner"
         style={{
@@ -30,15 +35,15 @@ console.log(movie);
         }}
         >
             <div className="banner_contents">
-            <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+            <h1 className="banner_title">{movie?.title || movie?.name || movie?.original_name}</h1>
             <div className="banner_buttons">
                 <button className="banner_button">Play</button>
                 <button className="banner_button">My List</button>
             </div>
             
-            <h1 className="banner_description">{movie?.overview}</h1>
+            <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
             </div>
-            {" "}
+        
          
         </header>
     )
