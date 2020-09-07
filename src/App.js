f746ae91b4945874d2f8074fe4f3cb54b56a70d1
Michelle Macato <michellefmacato@gmail.com>
2020-09-07 16:02:13 +0900

    added the banner and rows

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
+import requests from './request'; 
+import Banner from './Banner';
 
 function App() {
   return (
     <div className="App">
-    <h1>Hey Let's Build Netflix Clone</h1>
+    {}
+    <Banner />
+    <Row 
+    title="NETFLIX ORIGINALS" 
+    fetchUrl={requests.fetchNetflixOriginals}  
+    isLargeRow
+    /> 
+    <Row title="Trending Now" fetchUrl={requests.fetchTrending} /> 
+    <Row title="Top Rated" fetchUrl={requests.fetchTopRated} /> 
+    <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} /> 
+    <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} /> 
+    <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} /> 
+    <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} /> 
+
     </div>
   );
 }
